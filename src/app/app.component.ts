7540ed115418ffb0a08121c115a13285ff399d60
haha27lim <eileen2708@hotmail.com>
2023-03-25 03:44:24 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,23 +11,30 @@ export class AppComponent implements OnInit, AfterViewInit {
   form!: FormGroup
   items!: FormArray
 
+  // Initialize the FormBuilder dependency in the constructor
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    // create a new FormArray to store the items
     this.items = this.fb.array([])
+
+    // create a new FormGroup and set the 'items' property to the FormArray
     this.form = this.fb.group({
       items: this.items
     })
   }
 
+  // function to add a new item to the cart
   addToCart() {
+    // create a new FormGroup representing the item and add it to the FormArray
     this.items.push(this.createItem())
   }
 
+  // function to create a new FormGroup representing an item
   createItem() {
     return this.fb.group({
-      item: this.fb.control<string>(''),
-      quantity: this.fb.control<number>(1)
+      item: this.fb.control<string>(''), // create a FormControl for the item name
+      quantity: this.fb.control<number>(1) // create a FormControl for the item quantity
     })
   }
 
--- src/app/components/user-info.component.ts
+++ src/app/components/user-info.component.ts
@@ -8,12 +8,17 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })                                   
 export class UserInfoComponent implements OnInit {
 
+  // Declare a variable for the form
   form!: FormGroup
 
+  // Initialize the FormBuilder dependency in the constructor
   constructor(private fb: FormBuilder) { }
 
+  // Execute the code inside the method once the component is initialized
   ngOnInit(): void {
+    // Create a form group and assign it to the 'form' variable
     this.form = this.fb.group({
+      // Define form controls with default values
       name: this.fb.control(''),
       address: this.fb.control(''),
     })
